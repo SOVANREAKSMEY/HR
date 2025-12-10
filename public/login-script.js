0c66850abf2cc708a8dd740e887a6e24b290376c
SOVANREAKSMEY <wemedia24h@gmail.com>
2025-12-10 12:52:53 +0700

    Update login-script.js

--- public/login-script.js
+++ public/login-script.js
@@ -18,7 +18,7 @@ document.getElementById('login-form').addEventListener('submit', async (e) => {
   const errorMessage = document.getElementById('error-message');
   
   try {
-    const response = await fetch('http://localhost:3000/api/login', {
+    const response = await fetch('/api/login', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password })
